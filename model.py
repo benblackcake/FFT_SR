e3085c36afca3556dc6661aceb961e8fc2e1e54a
ben <pingheng.chang@gmail.com>
2020-03-23 23:05:36 +0800

    testing now input shape

--- model.py
+++ model.py
@@ -7,39 +7,29 @@ from utils import imshow,imshow_spectrum,plt_imshow
 
 class FFTSR:
 
-    def __init__(self, sess, learning_rate, epoch):
-        self.sess =sess
-        self.epoch = epoch
-        # self.images = images
+    def __init__(self, learning_rate=1e-4):
         self.learning_rate = learning_rate
-        self.build_model()
 
+    def loss_function(self,y, y_pred):
+        """
+        y is residual HR image
+        y_pred is network training result
+        """
+        return tf.nn.l2_loss(y - y_pred)
 
-    def build_model(self):
-        self.images = tf.placeholder(tf.float32, [256, 256], name='input_img')
-        self.label = tf.placeholder(tf.float32, [256, 256], name='HR_img')
+    def optimizer(self, loss_function):
+        """Tensorflow Adam Optimizer gradient"""
+        return tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss_function)
 
-        # self.image_matrix = tf.reshape(self.images, shape=[-1, 256, 256, 1])
-
-        self.pred = self.model()
-
-        label_residual = self.label - self.images
-        self.loss = tf.nn.l2_loss(label_residual - self.pred)
-        # squared_deltas = tf.square(self.label - self.pred)
-        # self.loss = L2_loss(self.label, self.pred)
-        # print(self.pred)
-        # self.loss = tf.reduce_mean(tf.square(label_residual - self.pred))
-        # print('build_model_image_shape',self.images)
-
-
-    def model(self):
+    def model(self, x):
         # x = None
-        f1,self.spatial_c1,self.spectral_c1 = self.fft_conv_pure(self.images,filters=5,width=256,height=256)
-        f2,self.spatial_c2,self.spectral_c2 = self.fft_conv_pure(f1,filters=5,width=256,height=256)
-        f3,self.spatial_c3,self.spectral_c3 = self.fft_conv_pure(f2,filters=5,width=256,height=256)
-        f4,self.spatial_c4,self.spectral_c4 = self.fft_conv_pure(f3,filters=5,width=256,height=256)
-        f5,self.spatial_c5,self.spectral_c5 = self.fft_conv_pure(f4,filters=5,width=256,height=256)
-        f6,self.spatial_c6,self.spectral_c6 = self.fft_conv_pure(f5,filters=5,width=256,height=256)
+        w, h= x.get_shape().as_list()
+        f1,self.spatial_c1,self.spectral_c1 = self.fft_conv_pure(x,filters=5,width=w,height=h)
+        f2,self.spatial_c2,self.spectral_c2 = self.fft_conv_pure(f1,filters=5,width=w,height=h)
+        f3,self.spatial_c3,self.spectral_c3 = self.fft_conv_pure(f2,filters=5,width=w,height=h)
+        f4,self.spatial_c4,self.spectral_c4 = self.fft_conv_pure(f3,filters=5,width=w,height=h)
+        f5,self.spatial_c5,self.spectral_c5 = self.fft_conv_pure(f4,filters=5,width=w,height=h)
+        f6,self.spatial_c6,self.spectral_c6 = self.fft_conv_pure(f5,filters=5,width=w,height=h)
 
         # f1_smooth,_,_ = self.fft_conv(f1,filters=5,width=5,height=5,stride=1,name='f1_smooth')
         # f_ = self.spectral_c1 +self.spectral_c2 +self.spectral_c3+self.spectral_c4 +self.spectral_c5+self.spectral_c6
@@ -53,7 +43,7 @@ class FFTSR:
     def fft_conv_pure(self, source, filters, width, height, activation='relu'):
         # This function applies the convolutional filter, which is stored in the spectral domain, as a element-wise
         # multiplication between the filter and the image (which has been transformed to the spectral domain)
-        source = tf.reshape(source,shape=[-1,256,256,1])
+        source = tf.reshape(source,shape=[-1,width,height,1])
         batch_size, input_height, input_width, depth = source.get_shape().as_list()
 
         # self.sess.run(tf.global_variables_initializer())
@@ -124,7 +114,7 @@ class FFTSR:
         # an alternative to this is to initialize directly in the spectral domain
         w = tf.truncated_normal([batch_size,height,width,filters], mean=0, stddev=0.01)
         fft_ = tf.fft2d(tf.complex(w, 0.0 * w), name='spectral_initializer')
-        return fft_.eval(session=self.sess)
+        return fft_.eval(session=tf.Session())
 
     def batch_fftshift2d(self, tensor):
         # Shifts high frequency elements into the center of the filter
@@ -146,29 +136,42 @@ class FFTSR:
 
         return tensor
 
-    def run(self,hr_img,lr_img):
-        self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-        self.sess.run(tf.global_variables_initializer())
-        print('run: ->',hr_img.shape)
-        # shape = np.zeros(hr_img.shape)
-        # err_ = []
-        # print(shape)
-        for er in range(self.epoch):
-            # image = tf.reshape(image,[image.shape[0],image.shape[1]])
-            _,x = self.sess.run([self.train_op,self.loss],feed_dict={self.images: lr_img, self.label:hr_img})
+    ###            old_area              ###
+    # def build_model(self):
+    #     self.images = tf.placeholder(tf.float32, [256, 256], name='input_img')
+    #     self.label = tf.placeholder(tf.float32, [256, 256], name='HR_img')
+    #
+    #     # self.image_matrix = tf.reshape(self.images, shape=[-1, 256, 256, 1])
+    #
+    #     self.pred = self.model()
+    #
+    #     label_residual = self.label - self.images
+    #     self.loss = tf.nn.l2_loss(label_residual - self.pred)
 
 
-            print(x)
-
-        result = self.pred.eval({self.images: lr_img})
-        result = result*255/(1e3*1e-5)
-        imshow_spectrum(self.sess.run(tf.fft2d(result)))
-        # plt_imshow(result)
-        result = np.clip(result, 0.0, 255.0).astype(np.uint8)
-        imshow(result)
-        plt_imshow(result)
-        lr = self.sess.run([self.images],feed_dict={self.images: lr_img, self.label:hr_img})
-        print(result+(np.asarray(lr)*255/(1e3*1e-5)))
-        plt_imshow(result+(np.asarray(np.squeeze(lr))*255/(1e3*1e-5)))
-
-        return result
+            # def run(self,hr_img,lr_img):
+    #     self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
+    #     self.sess.run(tf.global_variables_initializer())
+    #     print('run: ->',hr_img.shape)
+    #     # shape = np.zeros(hr_img.shape)
+    #     # err_ = []
+    #     # print(shape)
+    #     for er in range(self.epoch):
+    #         # image = tf.reshape(image,[image.shape[0],image.shape[1]])
+    #         _,x = self.sess.run([self.train_op,self.loss],feed_dict={self.images: lr_img, self.label:hr_img})
+    #
+    #
+    #         print(x)
+    #
+    #     result = self.pred.eval({self.images: lr_img})
+    #     result = result*255/(1e3*1e-5)
+    #     imshow_spectrum(self.sess.run(tf.fft2d(result)))
+    #     # plt_imshow(result)
+    #     result = np.clip(result, 0.0, 255.0).astype(np.uint8)
+    #     imshow(result)
+    #     plt_imshow(result)
+    #     lr = self.sess.run([self.images],feed_dict={self.images: lr_img, self.label:hr_img})
+    #     print(result+(np.asarray(lr)*255/(1e3*1e-5)))
+    #     plt_imshow(result+(np.asarray(np.squeeze(lr))*255/(1e3*1e-5)))
+    #
+    #     return result
--- train.py
+++ train.py
@@ -12,46 +12,87 @@ from matplotlib import pyplot as plt
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--learning-rate', type=float, default=1e-4, help='Learning rate for Adam.')
-    parser.add_argument('--epoch', type=int, default='1000000', help='How many iterations ')
+    parser.add_argument('--epoch', type=int, default='10000', help='How many iterations ')
     args = parser.parse_args()
 
+    lr_images = tf.placeholder(tf.float32, [None,None], name='lr_images')
+    hr_images = tf.placeholder(tf.float32, [None, None], name='hr_images')
 
-if __name__ == '__main__':
+    init_feed_ = tf.Variable(tf.ones([10,10]))
+    print(init_feed_.shape)
     img = 'images_train/butterfly.bmp'
     # img = cv2.imread(img,cv2.IMREAD_GRAYSCALE)
     img = cv2.imread(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-    print('origin_img_shape',img.shape)
+    hr_img = (img)/255.0 *(1e3*1e-5)
+    lr_img = (up_sample(bicubic(img)))/255.0 *(1e3*1e-5)
+    # lr_img = (up_sample(bicubic(img)))
+
+
+    fftsr = FFTSR(learning_rate=args.learning_rate)
 
 
 
-    # img = img.reshape([1,256,256,1])
     with tf.Session() as sess:
-        hr_img = (img)/255.0 *(1e3*1e-5)
-        lr_img = (up_sample(bicubic(img)))/255.0 *(1e3*1e-5)
-        # lr_img = (up_sample(bicubic(img)))
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+
+        lr = sess.run(lr_images,feed_dict={lr_images: lr_img[:, :, 0]})
+        hr = sess.run(hr_images,feed_dict={hr_images: hr_img[:, :, 0]})
+        lr = tf.Variable(lr)
+        hr = tf.Variable(hr)
+
+        sr_forward = fftsr.model(lr)
+        # sr_forward = fftsr.model(lr_images)
+        loss = fftsr.loss_function(hr_images - lr_images, sr_forward)
+        sr_opt = fftsr.optimizer(loss)
+        print('lr_images',lr_images)
+        print('hr_images',hr_images)
+        print(lr)
+        print(hr)
+
+        _, err = sess.run([sr_opt, loss],
+                          feed_dict={lr_images: lr, hr_images: hr})
 
 
-        # # imshow_spectrum(lr_img)
-        fftsr = FFTSR(sess, 1e-4, 15000)
+if __name__ == '__main__':
 
-        # fftsr.build_model()
-        res = fftsr.run(hr_img[:, :, 0], lr_img[:, :, 0])
-        sr_img = lr_img
-        print('res shape: ',res.shape)
-        sr_img = sr_img*255/(1e3*1e-5)
-        lr_img = lr_img*255/(1e3*1e-5)
-        print('sr_img[:,:,0]',sr_img[:,:,0])
-        print('sr_img[:,:,0].shape',sr_img[:,:,0].shape)
-        sr_img[:,:,0] = sr_img[:,:,0] + res
-        print('after add result',sr_img[:,:,0])
-        print('sr_img[:,:,0].shape',sr_img[:,:,0].shape)
-
-        print(lr_img.shape)
-        sr_img = sr_img.astype(np.uint8)
-        lr_img = lr_img.astype(np.uint8)
-        sr_img = cv2.cvtColor(sr_img, cv2.COLOR_YCR_CB2RGB)
-        lr_img = cv2.cvtColor(lr_img, cv2.COLOR_YCR_CB2RGB)
-
-        plt_imshow(sr_img)
-        plt_imshow(lr_img)
+    main()
+    # img = 'images_train/butterfly.bmp'
+    # # img = cv2.imread(img,cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread(img)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    # print('origin_img_shape',img.shape)
+    #
+    #
+    #
+    # # img = img.reshape([1,256,256,1])
+    # with tf.Session() as sess:
+    #     hr_img = (img)/255.0 *(1e3*1e-5)
+    #     lr_img = (up_sample(bicubic(img)))/255.0 *(1e3*1e-5)
+    #     # lr_img = (up_sample(bicubic(img)))
+    #
+    #
+    #     # # imshow_spectrum(lr_img)
+    #     fftsr = FFTSR(sess, 1e-4, 15000)
+    #
+    #     # fftsr.build_model()
+    #     res = fftsr.run(hr_img[:, :, 0], lr_img[:, :, 0])
+    #     sr_img = lr_img
+    #     print('res shape: ',res.shape)
+    #     sr_img = sr_img*255/(1e3*1e-5)
+    #     lr_img = lr_img*255/(1e3*1e-5)
+    #     print('sr_img[:,:,0]',sr_img[:,:,0])
+    #     print('sr_img[:,:,0].shape',sr_img[:,:,0].shape)
+    #     sr_img[:,:,0] = sr_img[:,:,0] + res
+    #     print('after add result',sr_img[:,:,0])
+    #     print('sr_img[:,:,0].shape',sr_img[:,:,0].shape)
+    #
+    #     print(lr_img.shape)
+    #     sr_img = sr_img.astype(np.uint8)
+    #     lr_img = lr_img.astype(np.uint8)
+    #     sr_img = cv2.cvtColor(sr_img, cv2.COLOR_YCR_CB2RGB)
+    #     lr_img = cv2.cvtColor(lr_img, cv2.COLOR_YCR_CB2RGB)
+    #
+    #     plt_imshow(sr_img)
+    #     plt_imshow(lr_img)
