d388c610a8a9635f15fe4a5a70c7847e1faca999
ben <pingheng.chang@gmail.com>
2020-03-28 14:30:26 +0800

    adjust model layer sum of spectral domin feature map

--- model.py
+++ model.py
@@ -37,10 +37,10 @@ class FFTSR:
         # f6,self.spatial_c6,self.spectral_c6 = self.fft_conv_pure(f5,filters=5,width=w,height=h, batch_size = batch_size)
 
         # f1_smooth,_,_ = self.fft_conv(f1,filters=5,width=5,height=5,stride=1,name='f1_smooth')
-        # f_ = self.spectral_c1 +self.spectral_c2 +self.spectral_c3+self.spectral_c4 +self.spectral_c5+self.spectral_c6
-        f_ = f1+f2+f3#+f4+f5+f6
-        f_ = f_ * tf.abs(tf.ifft2d(self.spectral_c3))
-        # f_ = tf.abs(tf.ifft2d(f_))
+        f_ = self.spectral_c1 +self.spectral_c2 +self.spectral_c3+self.spectral_c4 +self.spectral_c5+self.spectral_c6
+        # f_ = f1+f2+f3#+f4+f5+f6
+        # f_ = f_ * tf.abs(tf.ifft2d(self.spectral_c3))
+        f_ = tf.abs(tf.ifft2d(f_))
         print('__debug__spatial_c1',self.spatial_c1)
         return f_
 
